750a7daab9ec9b33fa611d52435b75c13c7191c5
Gustavo de Oliveira <gustavo100000.go@gmail.com>
2026-02-07 00:45:05 +0000

    First commit and first code in java

--- Exercicio01.java
+++ Exercicio01.java
@@ -1,7 +1,16 @@
-public class Exercicio01
-{
-    public static void main (String[] args)
-    {
+import java.util.Scanner;
+public class Exercicio01{
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner (System.in);
+        int idade;
+
         System.out.println("Olá, professor!");
+        System.out.println("Qual a sua idade?");
+
+        // idade = scan.nextInt();
+        // System.out.println("sua idade é: " + idade + " !");
+        int a = scan.nextInt();
+        System.out.println("baaaah");
     }
 }
